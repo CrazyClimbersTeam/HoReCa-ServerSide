65336a7e17e68af7570f83692eb4e0b182d9ce48
Vitalii Morozov <vmorozov@lohika.com>
2015-10-29 18:05:34 +0200

    No root password on DB; formatting changes

--- src/main/java/com/crazyclimbers/repository/PlacesRepository.java
+++ src/main/java/com/crazyclimbers/repository/PlacesRepository.java
@@ -15,19 +15,26 @@ import java.util.List;
 public interface PlacesRepository extends JpaRepository<Place, Long> {
     List<Place> findAll();
 
-    @Query(value = "SELECT DISTINCT places.* FROM places INNER JOIN menuitems ON places.id = menuitems.places_id WHERE menuitems.itemsCategories_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT places.* FROM places INNER JOIN" +
+            " menuitems ON places.id = menuitems.places_id" +
+            " WHERE menuitems.itemsCategories_id = ?1", nativeQuery = true)
     List<Place> placeByCategoryId(Long id);
 
-    @Query(value = "SELECT DISTINCT places.* FROM places INNER JOIN menuitems ON places.id = menuitems.places_id INNER JOIN itemscategories ON menuitems.itemsCategories_id = itemscategories.id WHERE itemscategories.name IN (?1)", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT places.* FROM places INNER JOIN menuitems ON places.id = menuitems.places_id " +
+            "INNER JOIN itemscategories ON menuitems.itemsCategories_id = itemscategories.id " +
+            "WHERE itemscategories.name IN (?1)", nativeQuery = true)
     List<Place> placeByCategoryNameList(List<String> listname);
 
-    @Query(value = "SELECT DISTINCT places.* FROM places WHERE places.short_descr like ?1", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT places.* FROM places" +
+            " WHERE places.short_descr like ?1", nativeQuery = true)
     List<Place> placeByCategoryDescription(String desc);
 
-    @Query(value = "SELECT menuitems.* FROM menuitems WHERE menuitems.places_id=?1", nativeQuery = true)
-    List<MenuItem> menuByPlaceId (Long id);
+    @Query(value = "SELECT menuitems.* FROM menuitems" +
+            " WHERE menuitems.places_id=?1", nativeQuery = true)
+    List<MenuItem> menuByPlaceId(Long id);
 
-    @Query(value = "SELECT propositions.* FROM propositions", nativeQuery = true)
+    @Query(value = "SELECT propositions.* " +
+            "FROM propositions", nativeQuery = true)
     List<Proposition> specialProposition();
 
 
